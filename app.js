219708a256dc4736dcf1c40055eae4cd13c24b91
narasimha <lakshminarasimha72692@gmail.com>
2023-09-02 07:37:31 +0530

    added post request

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 //app.method(endpoint(string), callback function);
 app.get("/home", (req, res) => {
     res.send("<h1>Hello Browser</h1>");
+});
+
+app.post("/home", (req, res) => {
+    res.send("<h1>Hello Request</h1>");
 })
 
 app.listen(8080, () => console.log("server is up at 8080"));
